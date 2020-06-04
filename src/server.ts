5e750281400e2417f8dcce2d5a59cfaaeef4774f
cleisonsantos <css.cleison@outlook.com>
2020-06-03 23:39:19 -0300

    Separado as rotas em um arquivo.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,44 @@
 import express from 'express';
-
+import routes from './routes'
 // No typescript é necessário a biblioteca de definições de tipos ex: @types/biblioteca
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-
-    response.json([
-        'Diego',
-        'Cleyton',
-        'Robson',
-        'Daniel'
-    ])
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
+
+
+
+/*
+const users = [
+    'Diego',
+    'Cleyton',
+    'Robson',
+    'Daniel'
+];
+
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
+
+app.post('/users', (request, respose) => {
+
+    const data = request.body;
+
+    console.log(data);
+
+    const user = {
+        name: 'Cleison',
+        email: 'css.cleison@outlook.com'
+    };
+
+    return respose.json(data);
+});
+*/
